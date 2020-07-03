6f22d1a4199e0a6caa68a9184436dac6867d8894
RuslanDudnikov <jerryletehen@gmail.com>
2020-07-03 14:29:10 +0300

    make ball begin move in random direction each start

--- Assets/Scripts/BallBehavior.cs
+++ Assets/Scripts/BallBehavior.cs
@@ -3,18 +3,29 @@
 public class BallBehavior : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    public Vector3 startVelocity;
-    public Vector3 startPos;
+    [SerializeField] private float startPower = 5f;
+    private Vector3 startPos;
 
     void Start()
     {
         startPos = transform.position;
-        rb.velocity = startVelocity;
+        Restart();
     }
 
     public void Restart()
     {
         transform.position = startPos;
-        rb.velocity = startVelocity;
+        rb.velocity = GetRandomVelocity();
+    }
+
+    private Vector3 GetRandomVelocity()
+    {
+        var result = Vector3.zero;
+        while (result.Equals(Vector3.zero) || result.Equals(Vector3.right) || result.Equals(Vector3.left) || result.Equals(Vector3.up) || result.Equals(Vector3.down))
+        {
+            result = new Vector3(Random.Range(-1f,1f), Random.Range(-1f,1f), 0f);
+        }
+
+        return result.normalized * startPower;
     }
 }
