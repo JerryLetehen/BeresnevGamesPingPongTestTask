d14d5518830d316b621efeb046f43809cb85099f
RuslanDudnikov <jerryletehen@gmail.com>
2020-07-09 22:12:45 +0300

    adapt for mobile, add csc.rsp

--- Assets/Scripts/Game/Ball/BallKicker/PingPongBallKicker.cs
+++ Assets/Scripts/Game/Ball/BallKicker/PingPongBallKicker.cs
@@ -1,6 +1,7 @@
 using PingPong.Datas.PingPingBallKickDatas;
 using PingPong.Game.Interfaces;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace PingPong
 {
@@ -8,7 +9,8 @@ namespace PingPong
     public class PingPongBallKicker : ScriptableObject, IBallKicker<IBall>
     {
         [SerializeField] private float defaultBallImpulse;
-        
+        [SerializeField, Range(30f, 89f)] private float deniedKickAngle = 45f;
+
         private PingPongBallKickData BallKickData => ballKickData ?? (ballKickData = new PingPongBallKickData());
 
         private PingPongBallKickData ballKickData;
@@ -19,11 +21,11 @@ namespace PingPong
             BallKickData.SetImpulse(defaultBallImpulse);
             ball.Kick(BallKickData);
         }
-        
+
         private Vector3 GetRandomDirection()
         {
             var result = Vector3.zero;
-            while (result.Equals(Vector3.zero) || result.Equals(Vector3.right) || result.Equals(Vector3.left) || result.Equals(Vector3.up) || result.Equals(Vector3.down))
+            while (Vector3.Angle(Vector3.right, new Vector3(Mathf.Abs(result.x), Mathf.Abs(result.y))) < deniedKickAngle)
             {
                 result = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
             }
